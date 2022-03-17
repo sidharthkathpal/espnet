fdb42b650aba2bb7b17b9db394ca305cadd1352f
sidharthkathpal <kathpal.sid@gmail.com>
2022-03-17 20:26:25 +0000

    adding feats_normalize in our run.sh for fused frontends

--- egs2/tedx_spanish/asr1/run.sh
+++ egs2/tedx_spanish/asr1/run.sh
@@ -11,7 +11,7 @@ train_set=train_"$(echo "${lang}" | tr - _)"
 train_dev=dev_"$(echo "${lang}" | tr - _)"
 test_set="${train_dev} test_$(echo ${lang} | tr - _)"
 
-asr_config=conf/tuning/train_asr_conformer5.yaml
+asr_config=conf/tuning/train_asr_fused_frontends.yaml
 lm_config=conf/train_lm.yaml
 inference_config=conf/decode_asr.yaml
 
@@ -41,5 +41,6 @@ fi
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
     --bpe_train_text "data/${train_set}/text" \
-    --lm_train_text "data/${train_set}/text" "$@"
+    --lm_train_text "data/${train_set}/text" "$@" \
+    --feats_normalize uttmvn
 
